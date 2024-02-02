4ccd7875d85f0345497609469d946be0298c0150
mayank parihar <parihar.mayank28@gmail.com>
2024-02-02 18:20:30 +0530

    index.js doc update

--- src/index.js
+++ src/index.js
@@ -1,15 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter } from "react-router-dom";
+import store from "./store";
+import { Provider } from "react-redux";
 
+store.subscribe(()=>console.log(store.getState()))
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
-    <App />
+  <Provider store={store}>
+      <App />
+  </Provider>
   </BrowserRouter>
 );
 
